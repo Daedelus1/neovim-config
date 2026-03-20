d5be853b066c8387034b46b16f9cc55958b03701
Daedelus <daedelusstormbow@gmail.com>
2026-03-20 15:31:17 -0400

    fixed automatic run configurations

--- init.lua
+++ init.lua
@@ -59,7 +59,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   callback = function(args)
     local ft = vim.bo[args.buf].filetype
-    -- require('fidget').notify('Filetype is ' .. ft)
+    --require('fidget').notify('Filetype is ' .. ft)
     if ft == 'rust' then
       vim.g.run_code_command = 'RustLsp runnables'
       vim.g.test_code_command = 'RustLsp testables'
@@ -75,11 +75,12 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
         update_in_insert = false, -- don't update while typing
         virtual_text = true,
       })
+    else
+      vim.g.run_code_command = "lua require('fidget').notify 'No run configuration set!'"
+      vim.g.test_code_command = "lua require('fidget').notify 'No test configuration set!'"
+      vim.g.build_code_command = "lua require('fidget').notify 'No build configuration set!'"
+      vim.g.clean_code_command = "lua require('fidget').notify 'No clean configuration set!'"
     end
-    vim.g.run_code_command = "lua require('fidget').notify 'No run configuration set!'"
-    vim.g.test_code_command = "lua require('fidget').notify 'No test configuration set!'"
-    vim.g.build_code_command = "lua require('fidget').notify 'No build configuration set!'"
-    vim.g.clean_code_command = "lua require('fidget').notify 'No clean configuration set!'"
     if ft ~= 'tex' then
       vim.g.vimtex_word_count_status_line_cache = ''
     end
@@ -630,9 +631,6 @@ require("lspconfig").nil_ls.setup({
   ft = "nix"
 })
 
-require("lspconfig").rust_analyzer.setup({
-  ft = "rs"
-})
 
 require("lspconfig").ltex.setup({
   ft = "tex",
@@ -647,33 +645,7 @@ require("lspconfig").ltex.setup({
     }
   }
 })
-
-
-
--- Brief aside: **What is LSP?**
---
--- LSP is an initialism you've probably heard, but might not understand what it is.
---
--- LSP stands for Language Server Protocol. It's a protocol that helps editors
--- and language tooling communicate in a standardized fashion.
---
--- In general, you have a "server" which is some tool built to understand a particular
--- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
--- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
--- processes that communicate with some "client" - in this case, Neovim!
---
--- LSP provides Neovim with features like:
---  - Go to definition
---  - Find references
---  - Autocompletion
---  - Symbol Search
---  - and more!
---
--- Thus, Language Servers are external tools that must be installed separately from
--- Neovim. This is where `mason` and related plugins come into play.
---
--- If you're wondering about lsp vs treesitter, you can check out the wonderfully
--- and elegantly composed help section, `:help lsp-vs-treesitter`
+require("lspconfig").texlab.setup({})
 
 --  This function gets run when an LSP attaches to a particular buffer.
 --    That is to say, every time a new file is opened that is associated with
